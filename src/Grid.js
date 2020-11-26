9108a0d678bb06f4a31c05891343e4731c785b24
bella <bella.royle@gmail.com>
2020-11-26 17:05:14 +0000

    made a nicer way to create the grid, and hover over is a colour only if the grid is empty

--- src/Grid.js
+++ src/Grid.js
@@ -4,57 +4,44 @@ import React from 'react';
 class Grid extends React.Component {
     state = {
         grid: ["", "", "", "", "", "", "", "", ""],
-        turn: true
+        turn: true,
+        class: ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"]
     }
 
     takeTurn(index) {
-            this.setState((currState) => {
-                if (currState.grid[index] === "") {
-                    const newGrid = [...currState.grid]
-                    newGrid[index] = (currState.turn ? "X" : "O")
-                    return {grid: newGrid, turn: !currState.turn}
-                }
-            })
-        }
+        this.setState((currState) => {
+            if (currState.grid[index] === "") {
+                const newGrid = [...currState.grid]
+                const newClass = [...currState.class]
+                newGrid[index] = (currState.turn ? "X" : "O")
+                newClass[index] = "filled"
+                return { grid: newGrid, turn: !currState.turn, class: newClass }
+            }
+        })
+    }
 
     render() {
         return (
-            <div id="grid">
-            <div class="row">
-                <div id={0} class="square" onClick={()=>this.takeTurn(0)}><p>{this.state.grid[0]}</p></div>
-                <div id={1} class="square" onClick={()=>this.takeTurn(1)}><p>{this.state.grid[1]}</p></div>
-                <div id={2} class="square" onClick={()=>this.takeTurn(2)}><p>{this.state.grid[2]}</p></div>
+            <div id="grid">{
+                this.state.grid.map((item, index) => {
+                    return <div id={index} class={`square ${this.state.class[index]}`} onClick={() => this.takeTurn(index)}><p>{this.state.grid[index]}</p></div>
+                })
+            }
             </div>
-            <div class="row">
-                <div id={3} class="square" onClick={()=>this.takeTurn(3)}><p>{this.state.grid[3]}</p></div>
-                <div id={4} class="square" onClick={()=>this.takeTurn(4)}><p>{this.state.grid[4]}</p></div>
-                <div id={5} class="square" onClick={()=>this.takeTurn(5)}><p>{this.state.grid[5]}</p></div>
-            </div>
-            <div class="row">
-                <div id={6} class="square" onClick={()=>this.takeTurn(6)}><p>{this.state.grid[6]}</p></div>
-                <div id={7} class="square" onClick={()=>this.takeTurn(7)}><p>{this.state.grid[7]}</p></div>
-                <div id={8} class="square" onClick={()=>this.takeTurn(8)}><p>{this.state.grid[8]}</p></div>
-            </div>
-        
-        </div>
-        )  
+
+
+        )
     }
 
-    makeGrid(numOfRows) {
-        const grid = [];
-        for (let i = 0; i < numOfRows*numOfRows; i++) {
-            grid.push("")
-        }
-        this.setState({grid: grid})
-        
-        for (let i = 0; i < numOfRows; i++) {
-            <div class="row">
-                {for (let j = 0; j < numOfRows; j++) {
-                    
-                }}
-            </div>
-        } 
-    }
-     
+    // componentDidMount() {
+    //     const grid = [];
+    //     for (let i = 0; i < numOfRows*numOfRows; i++) {
+    //         grid.push("")
+    //     }
+    //     this.setState({grid: grid})
+    // }
+
+    // makeGrid(numOfRows) {
+
 }
 export default Grid;
